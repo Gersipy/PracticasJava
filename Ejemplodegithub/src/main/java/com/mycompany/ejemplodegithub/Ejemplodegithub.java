1a5a5102b08192a610cedf78f99948966893c65c
Gersipy <gersipy@gmail.com>
2023-03-14 08:35:09 -0600

    Primer commit

---
+++ Ejemplodegithub/src/main/java/com/mycompany/ejemplodegithub/Ejemplodegithub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejemplodegithub;
+
+/**
+ *
+ * @author gersi
+ */
+public class Ejemplodegithub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
